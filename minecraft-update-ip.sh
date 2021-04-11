1691ab6e817b8d4a63b6cc49dd7da35e4fb75ce6
Ritesh Bhavsar <ritesh.bhavsar@gmail.com>
2021-04-11 11:31:49 +0100

    zone id use var

--- minecraft-update-ip.sh
+++ minecraft-update-ip.sh
@@ -2,7 +2,7 @@
 
 PUBLIC_IP=`curl -s http://169.254.169.254/latest/meta-data/public-ipv4`
 DOMAIN_NAME="minecraft.cavecraft.co.uk"
-ZONE_ID="Z08895941Q4F1JYONJGY6"
+ZONE_ID="YOUR_ZONE_ID"
 CHANGEFILE=/opt/minecraft/minecraft-dns.json
 
 echo "Public IP is $PUBLIC_IP"
@@ -21,4 +21,4 @@ cat <<EOF > $CHANGEFILE
 }
 EOF
 
-/usr/local/bin/aws route53 change-resource-record-sets --hosted-zone-id Z08895941Q4F1JYONJGY6 --change-batch file://$CHANGEFILE
+/usr/local/bin/aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file://$CHANGEFILE
